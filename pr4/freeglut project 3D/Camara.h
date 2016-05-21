900fce77b30ab0345ff49496a53e6572a7e190ce
huig- <igago@ucm.es>
2016-05-22 01:44:38 +0200

    Viewport y zoom

---
+++ pr4/freeglut project 3D/Camara.h
@@ -0,0 +1,40 @@
+ 
+#ifndef CamaraH
+#define CamaraH
+
+#include "PuntoVector3D.h"
+ 
+class Camara {
+	private:
+		PuntoVector3D *eye, *up, *look, *u, *v, *n;				
+        GLfloat left, right, bottom, top, Near, Far;                
+        GLdouble fovy, aspect; 		 
+		GLfloat angleX, angleY, angleZ;
+		void setView();	
+		void setCameraCoordinateSystem();
+        void setModelViewMatrix();
+
+	public:				
+		Camara();		 
+                                                                     
+        void giraX();
+        void giraY();
+        void giraZ();
+        void lateral();
+        void frontal();
+        void cenital();
+        void rincon(); 
+	void setProjection(); 
+	void roll(float ang);
+        void pitch(float ang);
+        void yaw(float ang);      
+	void setAngle(GLfloat angle);
+	GLfloat getAngle();
+	GLfloat getLeft();
+	GLfloat getRight();
+	GLfloat getBottom();
+	GLfloat getTop();
+	GLfloat getNear();
+	GLfloat getFar();
+};
+#endif
